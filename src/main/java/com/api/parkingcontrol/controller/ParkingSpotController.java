03e502162c977c23d07e580ce9dca505e646cbfa
Nicolas <nicolastessuto@gmail.com>
2023-03-01 19:14:25 -0300

    criado dto para alocacao dos dados, criado metodo para adicao de vagas, e metodo que salva ao banco no repository

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -1,10 +1,17 @@
 package com.api.parkingcontrol.controller;
 
 
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController //Recebe as solicitações e aciona o service
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,6 +23,16 @@ public class ParkingSpotController {
     public ParkingSpotController(ParkingSpotService parkingSpotService) {
         this.parkingSpotService = parkingSpotService;
     }
+
+    @PostMapping("/addVaga")
+    public ResponseEntity<Object> savePArkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        var parkingSpotModel = new ParkingSpotModel(); //Instracia uma variável do tipo model
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel); //Converte meu DTO em model
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTF")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel)); //Retorna ao Http um status created e salva ao banco
+
+    }
+
 
 
 
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service //Camada intermediária entre o controller e o repository e onde ficam as regras de negócio
@@ -10,5 +12,10 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional //Garante o rowback caso os dados sejam perdidos no meio da transação.
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
